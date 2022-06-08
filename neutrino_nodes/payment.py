85aa93a9f7e6bbd239ae2add7f2cec9419a9a1a3
Zaphod Beeblebrox <brox42@gmail.com>
2022-06-08 11:54:28 +0300

    payment script added

---
+++ neutrino_nodes/payment.py
@@ -0,0 +1,13 @@
+import pywaves as pw
+import datetime as dt
+
+pw.setNode('http://localhost:6869', chain='mainnet')
+myAddress = pw.Address(privateKey='your_node_private_key')
+dappAddress = '3P9vKqQKjUdmpXAfiWau8krREYAY1Xr69pE'
+beneficiaryAddress = '3P_your_beneficiary_address'
+fee = pw.DEFAULT_INVOKE_SCRIPT_FEE
+amount = myAddress.balance() - fee
+result = myAddress.invokeScript(dappAddress, 'distributeMinerReward', params=[{"type": "string", "value": beneficiaryAddress}], payments=[{"assetId": None, "amount": amount}], feeAsset = None, txFee=fee)
+now = dt.datetime.now()
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
+print(result)

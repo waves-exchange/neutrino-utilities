1b21a9709eb33f331b0bfd7ace275d561b398f37
WavesFunnyNode <junky@osmanosmanov.com>
2022-07-21 20:26:44 +0300

    Update payment.py
    *check balance before do invoke and min waves
    *short term fixed amount of waves ( https://t.me/neutrino_protocol_news/268 )

--- neutrino_nodes/payment.py
+++ neutrino_nodes/payment.py
@@ -6,8 +6,19 @@ myAddress = pw.Address(privateKey='your_node_private_key')
 dappAddress = '3P9vKqQKjUdmpXAfiWau8krREYAY1Xr69pE'
 beneficiaryAddress = '3P_your_beneficiary_address'
 fee = pw.DEFAULT_INVOKE_SCRIPT_FEE
+
 amount = myAddress.balance() - fee
-result = myAddress.invokeScript(dappAddress, 'distributeMinerReward', params=[{"type": "string", "value": beneficiaryAddress}], payments=[{"assetId": None, "amount": amount}], feeAsset = None, txFee=fee)
+
+if amount > 2400000000:
+
+ result = myAddress.invokeScript(dappAddress, 'distributeMinerReward',
+params=[{"type": "string", "value": beneficiaryAddress}],
+payments=[{"assetId": None, "amount": 2400000000}], # fixed amount of 24 waves - short term
+feeAsset = None, txFee=fee)
+
+else:
+    result = "Not generated reward of 24 waves to do invoce - distributeMinerReward. Current balance of Waves: " + str(amount / 100000000)
+
 now = dt.datetime.now()
 print(now.strftime("%Y-%m-%d %H:%M:%S"))
 print(result)
